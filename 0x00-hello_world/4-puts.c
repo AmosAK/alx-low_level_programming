a951d0343fc812a4870af64abfe197e28a167153
AmosAK <amosfrancis09@gmail.com>
2022-10-13 16:03:01 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry Point
+ *
+ * Return: Always returns to zero by default
+ */ 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+    puts("Programming is like building a multilingual puzzle\n");
+    return(0);
 }	
 
