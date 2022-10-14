8575b3da9b41f07004539da02902b29af58bc7fd
AmosAK <amosfrancis09@gmail.com>
2022-10-14 16:46:38 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point for code
+ *
+ *Return: to zero if no error is encountered
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c = 'z'; c ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
