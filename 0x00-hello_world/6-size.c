5d0ed778384ce8a5d09bd1f4debc4813b64f7c8a
AmosAK <amosfrancis09@gmail.com>
2022-10-13 17:44:52 -0700

    alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,10 @@
  */
 int main(void)
 {
-	int i;
-	float f;
-	char c;
-	long int li;
-	long long int lli,
-
-	printf("Size of an int: %lu. \n", (unsigned long)sizeof(i));
-	printf("Size of a float: %lu. \n", (unsigned long)sizeof(f));
-	printf("Size of a char: %lu. \n", (unsigned long)sizeof(c));
-	printf("Size of a long int: %lu. \n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu. \n", (unsigned long)sizeof(lli));
-	
+	printf("Size of a float: %lu. \n", (unsigned long)sizeof(float));
+	printf("Size of a char: %lu. \n", (unsigned long)sizeof(char));
+	printf("Size of a long int: %lu. \n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu. \n", (unsigned long)sizeof(long long int));
+	printf("Size of an int: %lu. \n", (unsigned long)sizeof(int));
 	return (0);
 }
