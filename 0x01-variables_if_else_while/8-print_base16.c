d3db78f54a1a19bbdae5e6744f23154bd928ac83
AmosAK <amosfrancis09@gmail.com>
2022-10-14 18:16:29 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-	int l[] = "0123456789";
-	char hexadec[] = "abcdef";
+	int l;
+	char hexavalues[] = "0123456789abcdef";
 
 	for (l = 10; l < 16; l++)
 	{
-		putchar(l[]);
-		{
-		putchar(hexadec[]);
-		}
+		putchar(hexavalues[l]);
 	}
 	putchar('\n');
 	return (0);
