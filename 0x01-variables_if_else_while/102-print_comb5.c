8e5b868699d7baebdbfdd3737c9f3aa3847954d7
AmosAK <amosfrancis09@gmail.com>
2022-10-18 01:52:05 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main- Entry point
+ *
+ *Return: Always zero if no error is encountered
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= 99, a++)
+	{
+		for (b = '0'; b <= 99, b++)
+		{
+			if (a < b)
+			{
+				putchar('0' + (a / 10));
+				putchar('0' + (a % 10));
+				putchar(' ');
+				putchar('0' + (b / 10));
+				putchar('0' + (b % 10));
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
