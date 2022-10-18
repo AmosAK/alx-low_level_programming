0b923ce106606e3d4deb875e9a4c56831d1ed436
AmosAK <amosfrancis09@gmail.com>
2022-10-18 13:56:27 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main- Entry point
+ *Description: Prints the word _Putchar, followed by new line
+ *Return: Always return to (0)
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int a;
+
+	for (a = 0; a <= 8; a++)
+		_putchar(word[a]);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README file created by Amos Akwemoh in writing and explaining loops and functions.
