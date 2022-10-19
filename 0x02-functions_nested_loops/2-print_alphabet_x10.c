ff495c7bb3fe026203d7fca3b3b8b522efaaac45
AmosAK <amosfrancis09@gmail.com>
2022-10-19 07:45:56 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main"
+
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in ls with newline
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = '0'; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			putchar(i);
+	}
+	putchar('\n);
+}
